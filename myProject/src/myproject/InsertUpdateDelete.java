f0614042a7873f787ac391c8c77322ff046b86e7
HP <HP@DESKTOP-K2T2V79>
2025-12-01 10:01:34 +0100

    my projects to be sent to github

---
+++ myProject/src/myproject/InsertUpdateDelete.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package myproject;
+
+import java.sql.Connection;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author HP
+ */
+public class InsertUpdateDelete {
+     public static void setData(String Query,String msg){
+        {
+            Connection con = null;
+            Statement st = null;
+            try
+            {
+                con=(Connection) ConnectionProvider.getCon();
+                st=con.createStatement();
+                st.executeUpdate(Query);
+                if(!msg.equals(""))
+                    JOptionPane.showMessageDialog(null, msg);
+                
+            }
+            catch(Exception e)
+            {
+                JOptionPane.showMessageDialog(null, e);
+            }
+            finally
+            {
+                try
+                {
+                    con.close();
+                    st.close();
+                }
+                catch(Exception e)
+                {}
+            }
+        }
+     }
+}
+        
+    
+
